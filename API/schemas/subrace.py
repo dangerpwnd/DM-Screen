0f013b6cb2e254e609af5beb907f8d23604ab589
Dangerpwnd <reric2012@gmail.com>
2020-09-11 09:19:01 -0500

    Updated class name to match casing method of other classes

--- API/schemas/subrace.py
+++ API/schemas/subrace.py
@@ -2,11 +2,12 @@ from marshmallow import Schema, fields, post_load
 from models.subrace import SubraceModel
 
 
-class SubraceSchema(Schema):
+class SubRaceSchema(Schema):
     id_subrace = fields.Integer(dump_only=True)
     subrace_name = fields.Str()
     subrace_descrip = fields.Str()
     race_id = fields.Integer()
+    race = fields.Nested("RaceSchema")
     features = fields.Nested("FeatureSchema", many=True)
     proficiencies = fields.Nested("ProficiencySchema", many=True)
 
