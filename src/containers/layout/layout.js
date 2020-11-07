3312fb35c439127b8142e4e9470aedbf6de4359c
Eric Dangerstone <edangerstone@pop-os.localdomain>
2020-11-06 23:26:19 -0600

    Built static nav

--- src/containers/layout/layout.js
+++ src/containers/layout/layout.js
@@ -3,6 +3,14 @@ import React, {Component} from 'react';
 
 class Layout extends Component {
     render() {
-
+        return (
+            <div>
+                <nav>
+                    <link>Home</link>
+                    <link>Player</link>
+                    <link>Party</link>
+                </nav>
+            </div>
+        )
     }
 }
