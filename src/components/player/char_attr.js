dced5b9756b5329514e1bd8710102a721c57cbae
Eric Dangerstone <edangerstone@pop-os.localdomain>
2020-11-10 21:51:48 -0600

    Added props to each attribute

--- src/components/player/char_attr.js
+++ src/components/player/char_attr.js
@@ -1,15 +1,15 @@
 import React from "react";
 
-const charAttr = () => {
+const charAttr = (props) => {
     return(
         <div className="playerAttrGrid">
             <div className="border">
-                <h3>Str: 14</h3>
-                <h3>Dex: 14</h3>
-                <h3>Con: 16</h3>
-                <h3>Int: 11</h3>
-                <h3>Wis: 13</h3>
-                <h3>Cha: 11</h3>
+                <h3>Str: {props.attr.str}</h3>
+                <h3>Dex: {props.attr.dex}</h3>
+                <h3>Con: {props.attr.con}</h3>
+                <h3>Int: {props.attr.int}</h3>
+                <h3>Wis: {props.attr.wis}</h3>
+                <h3>Cha: {props.attr.cha}</h3>
             </div>
         </div>
     )
