751ec90db76d2240f6eb004626b70c03eeb22f64
dangerpwnd <reric2012@gmail.com>
2021-07-03 22:46:17 -0500

    Use Layout rather than Player Details

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import './App.css';
-import PlayerDetail from './containers/player/player_detail';
 import Layout from './containers/layout/layout';
 
 
 function App() {
   return (
     <div className="App">
-        <PlayerDetail />
+        <Layout />
     </div>
   );
 }
